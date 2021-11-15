6763fe790d25e15e11083502e90c1a07ed424d50
Eduardo Ruiz <eduruiz333@gmail.com>
2021-11-15 10:39:57 -0300

    Refatoração para enxugar o código

--- foobar.js
+++ foobar.js
@@ -1,12 +1,11 @@
-const items = 30
-const foo = 'foo'
-const bar = 'bar'
-const foobar = foo + bar
-const result = []
 
-let print = document.querySelector('.print')
+(() => {
+  const items = 30,
+        result = [],
+        foo = 'foo',
+        bar = 'bar',
+        foobar = foo + bar
 
-function contador() {
   for (let i = 1; i <= items; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       result.push(foobar)
@@ -18,68 +17,16 @@ function contador() {
       result.push(i)
     }
   }
-  
-  print.innerHTML = result
-  
-  
 
-}
-contador()
-
-function makeList() {
   let listData = result,
-  // Make a container element for the list
-  listContainer = document.createElement('div'),
-  // Make the list
-  listElement = document.createElement('ul'),
-  // Set up a loop that goes through the items in listItems one at a time
-  numberOfListItems = listData.length,
-  listItem,
-  i;
+      list = document.querySelector('.list'),
+      listItem
 
-  // Add it to the page
-  document.getElementsByTagName('body')[0].appendChild(listContainer);
-  listContainer.appendChild(listElement);
-
-  for (i = 0; i < numberOfListItems; ++i) {
-      // create an item for each one
-      listItem = document.createElement('li');
-
-      // Add the item text
-      listItem.innerHTML = listData[i];
-
-      // Add listItem to the listElement
-      listElement.appendChild(listItem);
+  for (let i = 0; i < result.length; ++i) {
+    listItem = document.createElement('li')
+    listItem.innerHTML = listData[i]
+    list.appendChild(listItem)
   }
-}
-
-// Usage
-makeList();
+})()
 
 
-
-
-/* 
-COM FUNÇÃO ANÔNIMA 
-
-(() => {
-    const items = 60
-    const foo = 'foo'
-    const bar = 'bar'
-    const foobar = foo + bar
-    const result = []
-    for (let i = 1; i <= items; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            result.push(foobar)
-        } else if (i % 5 === 0) {
-            result.push(bar)
-        } else if (i % 3 === 0) {
-            result.push(foo)
-        } else {
-            result.push(i)
-        }
-    }
-    console.log(result)
-}) ()
-
-*/
