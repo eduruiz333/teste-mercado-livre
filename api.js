fe8be2bd53e51e6d1a44091572ce671c85c09859
Eduardo Ruiz <eduruiz333@gmail.com>
2021-11-15 13:27:46 -0300

    Implementação de estilo para marcar item selecionado

--- api.js
+++ api.js
@@ -13,31 +13,44 @@
                 list = document.querySelector('.list'),
                 listItem
 
+            //ALIMENTA O ARRAY COM A QUANTIDADE DE ITENS DESEJADO
             for (let i = 0; i <= items; i++) {
                 resultArray.push(productTitle[i].title)
             }
 
+            //UTILIZA OS DADOS DO ARRAY PARA POPULAR A LISTA, IMPLEMENTANDO AS REGRAS DE CSS PARA MÚLTIPLOS DE 3, 5, 3 E 5
             for (let i = 1; i < resultArray.length; ++i) {
 
                 if (i % 3 === 0 && i % 5 === 0) {
                     listItem = document.createElement('li')
                     listItem.innerHTML = listData[i]
-                    list.appendChild(listItem).classList.add('orange')
+                    list.appendChild(listItem).classList.add('prod', 'orange')
                 } else if (i % 5 === 0) {
                     listItem = document.createElement('li')
                     listItem.innerHTML = listData[i]
-                    list.appendChild(listItem).classList.add('red')
+                    list.appendChild(listItem).classList.add('prod', 'red')
                 } else if (i % 3 === 0) {
                     listItem = document.createElement('li')
                     listItem.innerHTML = listData[i]
-                    list.appendChild(listItem).classList.add('yellow')
+                    list.appendChild(listItem).classList.add('prod', 'yellow')
                 } else {
                     listItem = document.createElement('li')
                     listItem.innerHTML = listData[i]
-                    list.appendChild(listItem).classList.add('grey')
+                    list.appendChild(listItem).classList.add('prod', 'grey')
                 }
             }
 
+            // ADICIONA ESTILO PARA MARCAR ITEM SELECIONADO
+            const content = document.querySelector('.content');
+            const lista = content.getElementsByClassName('prod');
+            for (let i = 0; i < lista.length; i++) {
+                lista[i].addEventListener("click", function () {
+                    const current = document.getElementsByClassName("active");
+                    if (current.length > 0) {
+                        current[0].className = current[0].className.replace(" active", "");
+                    }
+                    this.className += " active";
+                });
+            }
         })
-
 })()
